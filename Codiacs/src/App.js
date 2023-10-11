04eae268767f18b35dbf390537f8d473b6a704ba
Thomas Banks <thomas.banks@univ.ox.ac.uk>
2023-10-12 00:19:31 +0100

    routing

--- Codiacs/src/App.js
+++ Codiacs/src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import LandingPage from "./landing-page/landing";
+import Canvas from "./canvas-app/canvas";
 import "./App.css";
 import VolcanoApp from "./volcano-app/VolcanoApp";
 
 function App() {
+  const [pageValue, setPageValue] = useState("landing");
+
   return (
     <div className="App">
-      <VolcanoApp />
+      {pageValue === "landing" ? (
+        <LandingPage setPageValue={setPageValue} />
+      ) : pageValue === "canvas" ? (
+        <Canvas setPageValue={setPageValue} />
+      ) : pageValue === "volcano" ? (
+        <VolcanoApp setPageValue={setPageValue} />
+      ) : (
+        <LandingPage setPageValue={setPageValue} />
+      )}
     </div>
   );
 }
