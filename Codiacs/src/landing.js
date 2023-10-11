c77110090378fd3767b2b973d33d1f50cc78e5d1
Matt Clayton <mattclayton9320@gmail.com>
2023-10-11 23:57:10 +0100

    save progress

--- Codiacs/src/landing.js
+++ Codiacs/src/landing.js
@@ -2,7 +2,7 @@ import './landing.css';
 function LandingPage(){
     return(
         <div class="container">
-            <a href="#" className="flex">
+            <button type="button" data-toggle="modal" data-target="#profile">
                 <div id="butterfly">
 
                     <div className="wing wingl">
@@ -26,59 +26,79 @@ function LandingPage(){
                     <div id="rightant"><div className="ball"></div></div>	
 
                 </div>
-	        </a>        
+            </button>
 
-      <div>
-        <img src="img/LISTENING EAR CMYK.jpg" alt="Listening Ear logo" />
-      </div>
+            <div class="modal fade" id="profile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+                </div>   
 
-      <div className="welcome_message">
-        <h1>
-          Welcome <span>Matt</span>
-        </h1>
-      </div>
-      <div className="row">
-        <div className="Game col">
-          <div href="">
-            <div className="content">
-              <div className="image" id="Memory_Jar">
-                <img src="img/MemoryJar2.png" alt="Memory Jar" />
-              </div>
-
-              <div className="text">
-                <p className="name">Memory Jar</p>
-              </div>
+            <div>
+                <img src="img/LISTENING EAR CMYK.jpg" alt="Listening Ear logo" />
             </div>
-          </div>
-        </div>
-        <div className="Game col">
-          <div href="">
-            <div className="content">
-              <div className="image" id="Canvas">
-                <img src="img/canvas.png" alt="Canvas" />
-              </div>
 
-              <div className="text">
-                <p className="name">Canvas</p>
-              </div>
+            <div className="welcome_message">
+                <h1>
+                Welcome <span>Matt</span>
+                </h1>
             </div>
-          </div>
-        </div>
-        <div className="Game col">
-          <div href="">
-            <div className="content">
-              <div className="image volcano">
-                <img src="img/volcano.png" alt="Volcano" />
-              </div>
+            <div className="row">
+                <div className="Game col">
+                    <div href="">
+                        <div className="content">
+                            <div className="image" id="Memory_Jar">
+                                <img src="img/MemoryJar2.png" alt="Memory Jar" />
+                            </div>
 
-              <div className="text">
-                <p className="name">Volcano</p>
-              </div>
+                            <div className="text">
+                                <p className="name">Memory Jar</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Game col">
+                    <div href="">
+                        <div className="content">
+                            <div className="image" id="Canvas">
+                                <img src="img/canvas.png" alt="Canvas" />
+                            </div>
+
+                            <div className="text">
+                                <p className="name">Canvas</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Game col">
+                    <div href="">
+                        <div className="content">
+                            <div className="image volcano">
+                                <img src="img/volcano.png" alt="Volcano" />
+                            </div>
+
+                            <div className="text">
+                                <p className="name">Volcano</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
   );
 }
 export default LandingPage;
